bb4f86e36044fc221a6c4b66607ef8a9a046442e
Cap'n Duck <coder@capnduck.com>
2023-12-29 08:48:53 -0800

    Update for tflint.

--- terraform.tf
+++ terraform.tf
@@ -6,16 +6,5 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 4.0"
     }
-
-    random = {
-      source  = "hashicorp/random"
-      version = ">= 3.0"
-    }
-
-    null = {
-      source  = "hashicorp/null"
-      version = ">= 3.0"
-    }
-
   }
 }
