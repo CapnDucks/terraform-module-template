9563234b9e701b1c3b309f4c912bc4feaab0f856
Cap'n Duck <coder@capnduck.com>
2023-12-28 16:09:11 -0800

    Updates.

--- variables.tf
+++ variables.tf
@@ -1,19 +1,20 @@
-# Every module should include these variables
+# Every module should include these variables.
+# They are currently commented out as tflint was throwing an error.
 
-variable "aws_region" {
-  default     = "us-east-1"
-  description = "AWS region to deploy to."
-  type        = string
-}
+#variable "aws_region" {
+#  default     = "us-east-1"
+#  description = "AWS region to deploy to."
+#  type        = string
+#}
 
-variable "tags" {
-  description = "A map of tags to assign to the resource."
-  type        = map(string)
-  default     = null
-}
+#variable "tags" {
+#  description = "A map of tags to assign to the resource."
+#  type        = map(string)
+#  default     = null
+#}
 
-variable "owner" {
-  default     = null
-  description = "Default owner for tagging purposes."
-  type        = string
-}
+#variable "owner" {
+#  default     = null
+#  description = "Default owner for tagging purposes."
+#  type        = string
+#}
