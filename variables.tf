e05926a9c08d6fc75ec0538221aa9ae9405891f9
Cap'n Duck <coder@capnduck.com>
2023-12-28 16:07:16 -0800

    Updates.

--- variables.tf
+++ variables.tf
@@ -1,15 +1,19 @@
+# Every module should include these variables
+
+variable "aws_region" {
+  default     = "us-east-1"
+  description = "AWS region to deploy to."
+  type        = string
+}
+
 variable "tags" {
   description = "A map of tags to assign to the resource."
   type        = map(string)
   default     = null
 }
 
-# These are here as examples ONLY - you can remove them
-
-#variable "vpc_name" {
-#  default = "capnduck*"
-#}
-
-#variable "domain_name" {
-#  default = "capnduck.com"
-#}
+variable "owner" {
+  default     = null
+  description = "Default owner for tagging purposes."
+  type        = string
+}
